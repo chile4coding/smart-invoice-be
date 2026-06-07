1949add5e4fbc6f89af67e620d574a67ea065277
chile4coding <chileomereji@gmail.com>
2026-06-07 13:25:07 +0100

    fix: configure CORS to allow specific frontend origin and enable credentials

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ const createApp = () => {
   const app = express();
 
   app.use(helmet({ contentSecurityPolicy: false }));
-  app.use(cors());
+  app.use(cors({
+  origin: [process.env['FRONTEND_URL']||"*"],
+  credentials: true,
+}));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
